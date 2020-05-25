962fcfd4d3a3d6924ed3007d1ce437664640ba94
João Lucas <joao.lucas@zage.com.br>
2020-05-24 23:40:50 -0300

    Removed old renderer stuff

--- main.c
+++ main.c
@@ -70,19 +70,6 @@ static void init(k_Executable* restrict self) {
         k_LogErrorf("Not enough memory to create a renderer\n");
         exit(EXIT_FAILURE);
     }
-
-    /*
-    glGenBuffers(1, &vertexBufferID);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
-    glBufferData(GL_ARRAY_BUFFER, 3 * 36 * sizeof(float), data, GL_STATIC_DRAW);
-
-    glGenBuffers(1, &colorBufferID);
-    glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
-    glBufferData(GL_ARRAY_BUFFER, 3 * 36 * sizeof(float), color, GL_STATIC_DRAW);
-
-    mvpID = glGetUniformLocation(g->program, "MVP");
-    rotID = glGetUniformLocation(g->program, "Transform");
-    */
 }
 
 static void step(k_Executable* restrict self, float dt) {
@@ -99,36 +86,6 @@ static void step(k_Executable* restrict self, float dt) {
     glUseProgram(g->program);
     k_RendererDrawTriangles(r, &rot, &g->mvp, data, sizeof(cube));
     k_RendererColorTriangles(r, color, sizeof(color));
-
-    /*
-    glUniformMatrix4fv(mvpID, 1, GL_FALSE, (const GLfloat*)&g->mvp.m[0][0]);
-    glUniformMatrix4fv(rotID, 1, GL_FALSE, (const GLfloat*)&rot.m[0][0]);
-
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
-    glVertexAttribPointer(
-        0,         // attribute 0. No particular reason for 0, but must match the layout in the shader.
-        3,         // size
-        GL_FLOAT,  // type
-        GL_FALSE,  // normalized?
-        0,         // stride
-        (void*)0   // array buffer offset
-    );
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-    glDisableVertexAttribArray(0);
-
-    glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
-    glVertexAttribPointer(
-        1,         // attribute. No particular reason for 1, but must match the layout in the shader.
-        3,         // size
-        GL_FLOAT,  // type
-        GL_FALSE,  // normalized?
-        0,         // stride
-        (void*)0   // array buffer offset
-    );
-    glDisableVertexAttribArray(1);
-    */
 }
 
 static GLuint loadShaderProgram() {
